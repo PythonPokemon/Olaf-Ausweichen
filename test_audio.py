34a6f4bdc928106944a421c95cf3e436783e304f
PythonPokemon <Jakob.D.Baron@Outlook.de>
2023-08-16 10:01:24 +0200

    first commit

---
+++ test_audio.py
@@ -0,0 +1,113 @@
+import pygame
+import time
+import random
+pygame.font.init()
+
+WIDTH, HEIGHT = 1000, 800
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Olaf Special")
+
+
+
+BG = pygame.transform.scale(pygame.image.load("schloss.png"), (WIDTH, HEIGHT)).convert_alpha()
+
+# Initialisiere den Audio Mixer
+pygame.mixer.init()
+
+# load audio
+pygame.mixer.music.load('olaf.mp3')
+
+PLAYER_WIDTH = 60
+PLAYER_HEIGHT = 80
+PLAYER_VEL = 5
+
+STAR_WIDTH = 30
+STAR_HEIGHT = 30
+STAR_VEL = 3
+
+FONT = pygame.font.SysFont("comicsans", 30)
+
+# Lade die Bilder für den Player und den Star
+player_img = pygame.image.load("olaf.png").convert_alpha()
+star_img = pygame.image.load("snowball.png").convert_alpha()
+
+# Skaliere die Bilder, um ihre Größe zu ändern
+player_img = pygame.transform.scale(player_img, (PLAYER_WIDTH, PLAYER_HEIGHT))
+star_img = pygame.transform.scale(star_img, (STAR_WIDTH, STAR_HEIGHT))
+
+def draw(player, elapsed_time, stars):
+    WIN.blit(BG, (0, 0))
+
+    time_text = FONT.render(f"Time: {round(elapsed_time)}s", 1, "white")
+    WIN.blit(time_text, (10, 10))
+
+    # Statt pygame.draw.rect() verwenden wir die Methode blit() mit den Bildern
+    WIN.blit(player_img, (player.x, player.y))
+
+    for star in stars:
+        WIN.blit(star_img, (star.x, star.y))
+
+    pygame.display.update()
+
+def main():
+    run = True
+
+    pygame.mixer.music.play(-1)  # -1 means play the audio in an infinite loop
+
+    player = pygame.Rect(200, HEIGHT - PLAYER_HEIGHT, PLAYER_WIDTH, PLAYER_HEIGHT)
+    clock = pygame.time.Clock()
+    start_time = time.time()
+    elapsed_time = 0
+
+    star_add_increment = 2000
+    star_count = 0
+
+    stars = []
+    hit = False
+
+    while run:
+        star_count += clock.tick(60)
+        elapsed_time = time.time() - start_time
+
+        if star_count > star_add_increment:
+            for _ in range(3):
+                star_x = random.randint(0, WIDTH - STAR_WIDTH)
+                star = pygame.Rect(star_x, -STAR_HEIGHT, STAR_WIDTH, STAR_HEIGHT)
+                stars.append(star)
+
+            star_add_increment = max(200, star_add_increment - 50)
+            star_count = 0
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT] and player.x - PLAYER_VEL >= 0:
+            player.x -= PLAYER_VEL
+        if keys[pygame.K_RIGHT] and player.x + PLAYER_VEL + player.width <= WIDTH:
+            player.x += PLAYER_VEL
+
+        for star in stars[:]:
+            star.y += STAR_VEL
+            if star.y > HEIGHT:
+                stars.remove(star)
+            elif star.y + star.height >= player.y and star.colliderect(player):
+                stars.remove(star)
+                hit = True
+                break
+
+        if hit:
+            lost_text = FONT.render("You Lost!", 1, "white")
+            WIN.blit(lost_text, (WIDTH/2 - lost_text.get_width()/2, HEIGHT/2 - lost_text.get_height()/2))
+            pygame.display.update()
+            pygame.time.delay(4000)
+            break
+
+        draw(player, elapsed_time, stars)
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
